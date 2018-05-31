75cd996966fce8a0bce3fa238bc4a7b00b1e4ffe
Matthew Sharpe <mattsharpe72@gmail.com>
2018-05-31 11:49:20 -0400

    form tweaks

--- dog_city/dogcity/forms.py
+++ dog_city/dogcity/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Owner, Dog, Walk
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms.widgets import CheckboxSelectMultiple
 
 class DogForm(forms.ModelForm):
     class Meta:
@@ -11,7 +12,14 @@ class DogForm(forms.ModelForm):
 class WalkForm(forms.ModelForm):
     class Meta:
         model = Walk
-        fields = ('day', 'date', 'dogs',)
+        fields = ('date', 'day', 'dogs',)
+        widgets = {'dogs': CheckboxSelectMultiple}
+
+        def __init__(self, *args, **kwargs):
+            super(WalkForm, self).__init__(*args, **kwargs)                    
+            self.fields['dogs'].widget = CheckboxSelectMultiple()
+            self.fields['dogs'].queryset = Dog.objects.all()
+        # fields = ('day', 'date', 'dogs',)
 
 class OwnerForm(forms.ModelForm):
     class Meta:
